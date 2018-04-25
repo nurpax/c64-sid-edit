5af81b737fd5a9252d4c1cfd13133868dbb3aa74
Janne Hellsten <jjhellst@gmail.com>
2018-04-25 22:31:42 +0300

    Use more Data.Binary.Get for parsing

--- GrabSounds.hs
+++ GrabSounds.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+-- HOW TO RUN IT:
+--
+-- stack runghc GrabSounds.hs
+
 import Data.Binary.Get
+import Control.Monad.Trans.Class
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy.Char8 as B
 import Data.Monoid ((<>))
@@ -10,17 +15,6 @@ import System.IO (Handle, IOMode(..), SeekMode(..), withBinaryFile, hSeek)
 assert :: Bool -> String -> IO ()
 assert True  _   = return ()
 assert False msg = error msg
-
-bytesToInt :: ByteString -> Integer
-bytesToInt bs = fromIntegral (b0 + b1*256 + b2*65536 + b3*65536*256)
-  where
-    b0 = ord . B.index bs $ 0
-    b1 = ord . B.index bs $ 1
-    b2 = ord . B.index bs $ 2
-    b3 = ord . B.index bs $ 3
-
-showByte :: ByteString -> ByteString
-showByte = B.pack . show . ord . B.head
 
 dumpSounds :: ByteString -> IO ()
 dumpSounds ram = do
@@ -32,54 +26,62 @@ dumpSounds ram = do
       getLazyByteString (fromIntegral paramLen)
     save bytes = do
       let filename = "sounds.bin"
-      putStrLn ("writing sound data to " <> filename)
+      putStrLn ("writing sound data to " <> filename <> " bytes " <> show (B.length bytes))
       B.writeFile "sounds.bin" bytes
 
 readC64RAM :: Handle -> IO ()
-readC64RAM h = do
-  B.putStrLn "Reading C64MEM"
-  hSeek h RelativeSeek 4 -- skip header bytes
-  ramBytes <- B.hGet h 0x10000
-  dumpSounds ramBytes
-  hSeek h RelativeSeek 15 -- skip footer bytes
+readC64RAM h = B.hGet h  (4+0x10000+15) >>= (\bs -> dumpSounds (runGet parseRAM bs))
+  where
+    parseRAM = do
+      skip 4
+      ram <- getLazyByteString 0x10000
+      skip 15
+      return ram
 
 readModules :: Handle -> IO ()
 readModules h = do
-  moduleName <- B.hGet h 16
-  if moduleName == B.empty then return ()
+  hdr <- B.hGet h (16+2+4)
+  if hdr == B.empty then
+    return ()
   else do
-    B.putStrLn ("module: " <> moduleName)
-    vmajor <- B.hGet h 1
-    vminor <- B.hGet h 1
-    B.putStrLn ("Module version: " <> showByte vmajor <> "." <> showByte vminor)
-    size <- ((+) (-22) . bytesToInt) <$> B.hGet h 4
-    putStrLn ("content size: " <> show size)
-    readModule (B.takeWhile (\c -> c /= '\0') moduleName) size
+    let (name, vmajor, vminor, size) = runGet parseModuleHeader hdr
+    B.putStrLn ("module: " <> name)
+    putStrLn ("  module version: " <> show vmajor <> "." <> show vminor)
+    putStrLn ("  content size: " <> show size)
+    readModule (B.takeWhile (\c -> c /= '\0') name) size
+    readModules h
   where
+    parseModuleHeader = do
+      mname  <- getLazyByteString 16
+      vmajor <- getWord8
+      vminor <- getWord8
+      size   <- getWord32le
+      return (mname, vmajor, vminor, fromIntegral size - 22)
+
     readModule "C64MEM" _ = do
       readC64RAM h
-      readModules h
     readModule _modname size = do
       hSeek h RelativeSeek size
-      readModules h
 
 readMagic :: Handle -> IO ()
 readMagic h = do
-  magic <- B.hGet h 19
-  B.putStrLn magic
-  assert (B.isPrefixOf "VICE Snapshot File" magic) "invalid magic"
-  vmajor <- B.hGet h 1
-  vminor <- B.hGet h 1
-  B.putStrLn ("File format version: " <> showByte vmajor <> "." <> showByte vminor)
-  _machineName <- B.hGet h 16
-  -- This part is undocumented in VICE docs. :( Figured this out by looking at
-  -- the file in hex editor and then reading VICE source code
+  -- The magic format doesn't actually match what's written in VICE docs. :(
+  -- Figured this out by looking at the file in hex editor and then reading
+  -- VICE source code
   -- (https://gist.github.com/nurpax/71aa57be0b819163bbf950715b41e3e0).
-  versionMagic <- B.hGet h 13
-  B.putStrLn magic
-  _viceVersion <- B.hGet h 8
+  magicHdr <- B.hGet h (19+1+1+16+13+8)
+  let (magic, vmajor, vminor, machine) = runGet parseHeader magicHdr
+  assert (B.isPrefixOf "VICE Snapshot File" magic) "invalid magic"
+  assert (B.isPrefixOf "C64" machine) "only c64 is supported"
+  putStrLn ("File format version: " <> show vmajor <> "." <> show vminor)
   readModules h
-  return ()
+  where
+    parseHeader = do
+      magic  <- getLazyByteString 19
+      vmajor <- getWord8
+      vminor <- getWord8
+      machineName <- getLazyByteString 16
+      return (magic, vmajor, vminor, machineName)
 
 main :: IO ()
 main = withBinaryFile "snapshot.vsf" ReadMode readMagic
